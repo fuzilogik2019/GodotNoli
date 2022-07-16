b44b9cb92b6fa361719d6d8d51ed78eed40ecffa
agustinoli <aolivaarias@gmail.com>
2022-07-16 16:09:06 -0300

    chore: fixed merge issues again

--- Bed/Bed.gd
+++ Bed/Bed.gd
@@ -6,8 +6,5 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.get_name() == 'Player':
 		if !body.lights:
-      SfxManager.play("won")
+			SfxManager.play("won")
 			get_parent().next_scene()
-
-			
-
